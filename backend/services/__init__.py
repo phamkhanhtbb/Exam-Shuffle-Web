45f48f1633f4debadd28ec6520b232c8854f5e08
phamkhanhtbb <24020175@vnu.edu.vn>
2026-02-11 15:50:37 +0700

    fix: correct services/__init__.py imports

--- backend/services/__init__.py
+++ backend/services/__init__.py
@@ -1,10 +1,14 @@
-from .aws_service import AwsService
+from .aws_service import AwsService, aws
 from .answer_parser import parse_answer_map_from_text, extract_answers_from_structure
-from .preview_service import PreviewService
+from .preview_service import process_preview, render_structure, render_element
 
 __all__ = [
     'AwsService',
+    'aws',
     'parse_answer_map_from_text',
     'extract_answers_from_structure',
-    'PreviewService',
+    'process_preview',
+    'render_structure',
+    'render_element',
 ]
+
